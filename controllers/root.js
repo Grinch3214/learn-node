ba50d6063cc877978ea71d42062a7e478d5d70df
Max <scorpiomax90@gmail.com>
2025-08-23 16:29:56 +0300

    Some refactoring code

---
+++ controllers/root.js
@@ -0,0 +1,3 @@
+export function getRootHandler(req, res) {
+  res.send('getRootHandler');
+}
--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 import express from 'express';
-import commentsRouter from './routes/comments.js';
+import router from './routes/index.js';
 
 const app = express();
 
 const PORT = 5000;
 
-function getRootHandler(req, res) {
-  res.send('getRootHandler');
-}
-
-app.get('/', getRootHandler);
-app.use('/comments', commentsRouter);
+app.use(router);
 
 app.listen(PORT, () => console.log(`Started server on port ${PORT}`));
