e77932128f36d881c97be48c287db14bfada1cc9
Max <scorpiomax90@gmail.com>
2025-08-23 15:12:57 +0300

    create get/post on express

--- index.js
+++ index.js
@@ -4,8 +4,20 @@ const app = express();
 
 const PORT = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Test 123 express');
-});
+function getRoothandler(req, res) {
+  res.send('getRoothandler');
+}
 
-app.listen(PORT);
+function getCommentsHandler(req, res) {
+  res.send('getCommentsHandler');
+}
+
+function postCommentsHandler(req, res) {
+  res.send('postCommentsHandler');
+}
+
+app.get('/', getRoothandler);
+app.get('/comments', getCommentsHandler);
+app.post('/comments', postCommentsHandler);
+
+app.listen(PORT, () => console.log(`Started server on port ${PORT}`));
